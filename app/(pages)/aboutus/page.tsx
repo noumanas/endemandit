c6a3c526817efe68e5c09a2547fdf943a9efe0fc
nouman qureshi <noumanqureshi15@gmail.com>
2024-02-24 16:04:23 +0500

    working on about page

--- app/(pages)/aboutus/page.tsx
+++ app/(pages)/aboutus/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
-
+import { ShapingTomorrow } from "@/app/components/ShapingTomorrow";
+import { GlobalReach } from "@/app/components/GlobalReach";
 const page = () => {
-  return <div>about us page</div>;
+  return <div>
+    <div className={`bg-[url(../public/assets/RectangleBackground.png)] bg-[#0f2152] py-10`}>
+    <ShapingTomorrow/>
+    </div>
+    <GlobalReach/>
+  </div>;
 };
 
 export default page;
---
+++ app/components/ShapingTomorrow.tsx
@@ -0,0 +1,29 @@
+import rightArrow from "../../public/assets/rightArrow.png";
+import Image from "next/image"
+import Imagecontainer from "../../public/assets/Imagecontainer.png"
+import Imagecontainer2 from "../../public/assets/Imagecontainer2.png"
+import strokeline from "../../public/assets/strokeline.png"
+
+export function ShapingTomorrow(){
+    return(
+        <div className="pt-9 lg:px-40 py-40">
+            <div className="px-[20px] text-center items-center flex flex-col lg:flex-row  justify-center lg:text-center ">
+                <div className="text-center lg:text-center items-center flex flex-col justify-center lg:items-center ">
+                    <h1 className="font-extrabold text-[36px] font-Inter text-[#ffffff] lg:text-[72px]">Shaping Tomorrow with </h1>
+                    <h1 className="font-extrabold text-[36px] font-Inter text-[#ffffff] lg:text-[72px]">EnDemand IT’s Expertise</h1>
+                    <Image className="relative" src={strokeline} width={250} alt="line"/>
+                    <p className="text-[#fff] font-bold text-[36px] leading-[36px] py-10">Crafting Digital Excellence Across Borders</p>
+                    <p className="text-[#fff] font-regular text-[18px] leading-[30px]">In the heart of the Middle East's rapidlyevolving digital landscape, EnDemand IT stands as a beacon of innovation andexcellence. Founded on the principles of technological advancement andcustomer-centric solutions, our mission is to empower businesses across SaudiArabia, UAE, and Qatar with cutting-edge digital transformation and effectiveresource augmentation.</p>
+                    <button className="mt-10 bg-[ABE03C] flex font-medium bg-[#ABE03C] h-[60px] w-[228px] items-center justify-center rounded-lg text-[#102356]" >
+                            Explore our services
+                            <Image src={rightArrow} className="ml-2" alt="rightArrow"/>
+                    </button>
+                </div>
+               
+                {/* <Image src={Imagecontainer2} alt="container" width={700} height={549} className=" lg:block mb-40 mt-20"/> */}
+
+
+            </div>
+        </div>
+    )
+}
