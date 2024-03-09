9eaa1ce5512b855198d8999ef13e7a069c96bac1
jainshaikh101 <zain@blacklionapp.xyz>
2024-03-10 00:23:53 +0500

    About Us Solve

--- app/(pages)/aboutus/page.tsx
+++ app/(pages)/aboutus/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { ShapingTomorrow } from "@/app/components/ShapingTomorrow";
 import { GlobalReach } from "@/app/components/GlobalReach";
-import {AboutContentOne} from "@/app/components/AboutContentOne"
+import { AboutContentOne } from "@/app/components/AboutContentOne";
 import { Shaping } from "@/app/components/Shaping";
 const page = () => {
   return (
@@ -11,23 +11,17 @@ const page = () => {
       >
         <ShapingTomorrow />
       </div>
-      <GlobalReach />
+      <div className="relative bottom-0 flex flex-col justify-center items-center -top-[180px]">
+        <AboutContentOne />
+      </div>
+      <div>
+        <Shaping />
+      </div>
+      <div className="relative py-20">
+        <GlobalReach />
+      </div>
     </div>
-  return <div>
-    <div className={`bg-[url(../public/assets/RectangleBackground.png)] bg-[#0f2152] py-10`}>
-    <ShapingTomorrow/>
-    </div>
-    <div className="relative bottom-0 flex flex-col justify-center items-center -top-[180px]">
-    <AboutContentOne/>
-    </div>
-    <div>
-      <Shaping/>
-    </div>
-  <div className="relative py-20">
-  <GlobalReach/>
-
-  </div>
-  </div>;
+  );
 };
 
 export default page;
